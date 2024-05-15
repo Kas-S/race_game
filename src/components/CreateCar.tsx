48a9e357ae16b5b7dede48c57c617591b7a22293
Dr Flame <drflame707@gmail.com>
2024-05-15 19:53:11 +0500

    Create Car Form component

---
+++ src/components/CreateCar.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+interface Props {
+    clickEvent: (name: string, color: string) => void
+}
+
+function CreateCar({ clickEvent }: Props) {
+    const [name, setName] = useState<string>(''),
+        [color, setColor] = useState<string>('')
+    return (
+        <form>
+            <input type="text" placeholder="Car name" onChange={(e) => setName(e.target.value)}/>
+            <input type="color" onChange={(e) => setColor(e.target.value)}/>
+            <button type="button" onClick={() => clickEvent(name, color)}>Create</button>
+        </form>
+    )
+}
+
+export default CreateCar
