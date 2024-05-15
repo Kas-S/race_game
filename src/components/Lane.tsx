be8c5c618dea788f1acfd9bd25447e69814fb4af
Dr Flame <drflame707@gmail.com>
2024-05-15 23:40:52 +0500

    Independent car start added

--- src/components/Lane.tsx
+++ src/components/Lane.tsx
@@ -1,5 +1,5 @@
 import Car from './Car.tsx'
-import React from "react"
+import React, {useState, useEffect} from "react"
 import {CarType} from '../utils/types.tsx'
 
 interface LaneProps {
@@ -14,6 +14,12 @@ interface LaneProps {
 }
 
 function Lane({carKey, carColor, carName, removeCar, selectCar, velocity, startTime, race}: LaneProps) {
+    const [localRace, setLocalRace] = useState<boolean>(false),
+          [localStartTime, setLocalStartTime] = useState<number>(0)
+    useEffect(() => {
+        setLocalRace(race)
+        setLocalStartTime(startTime)
+    }, [race, startTime]);
     return (
         <div className="lane">
             <div className="car-control-buttons">
@@ -23,13 +29,13 @@ function Lane({carKey, carColor, carName, removeCar, selectCar, velocity, startT
                     <button type="button" className="car-remove-button" onClick={() => removeCar(carKey)}>Remove</button>
                 </div>
                 <div>
-                    <button type="button" className="car-control-button">S</button>
+                    <button type="button" className="car-control-button" onClick={() => {setLocalRace(true); setLocalStartTime(Date.now())}}>S</button>
                     <br/><br/>
-                    <button type="button" className="car-control-button">R</button>
+                    <button type="button" className="car-control-button" onClick={() => setLocalRace(false)}>R</button>
                 </div>
             </div>
             <div className="track">
-                <Car color={carColor} velocity={velocity} startTime={startTime} race={race}/>
+                <Car color={carColor} velocity={velocity} startTime={localStartTime} race={localRace}/>
                 <h3 style={{color: "white"}}>{carName}</h3>
             </div>
         </div>
