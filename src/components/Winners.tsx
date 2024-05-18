f2de0b7fd3c6e8dfdab5a4bbeeeb5e9e9c1a3e49
Dr Flame <drflame707@gmail.com>
2024-05-18 19:08:34 +0500

    Winners table generation alpha version

--- src/components/Winners.tsx
+++ src/components/Winners.tsx
@@ -1,18 +1,48 @@
-import {useEffect} from "react";
-
+import {useEffect, useState} from "react";
+import {WinnerType} from "../utils/types.ts";
 
 function Winners() {
+    const [winners, setWinners] = useState<WinnerType[]>([])
+    const [carNames, setCarNames] = useState<string[]>([])
 
     useEffect(() => {
         fetch("http://127.0.0.1:3000/winners")
             .then(res => res.json())
             .then(data => {
-                console.log(data)
+                    setWinners(data)
             })
-    }, []);
+    }, [])
+
+    useEffect(() => {
+        setCarNames([])
+        winners.forEach(v => {
+            fetch(`http://127.0.0.1:3000/garage/${v.id}`)
+                .then(res => res.json())
+                .then(data => {
+                    setCarNames((prevState) => [...prevState, data.name])
+                })
+        })
+    }, [winners]);
 
     return (
-        <></>
+        <table>
+            <thead>
+                <tr>
+                    <th>Car name</th>
+                    <th>Wins</th>
+                    <th>Best time</th>
+                </tr>
+            </thead>
+            <tbody>
+                {winners && winners.map((winner, i) => (
+                    <tr key={winner.id + Math.random()}>
+                        <td>{carNames[i]}</td>
+                        <td>{winner.wins}</td>
+                        <td>{winner.time}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
     )
 }
 
