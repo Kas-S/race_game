9612ba235213c5af519f09bca24a34ba01db1a3a
Dr Flame <drflame707@gmail.com>
2024-05-18 18:41:39 +0500

    Winners page alpha version (winners are displayed in console

--- src/components/Winners.tsx
+++ src/components/Winners.tsx
@@ -1,4 +1,17 @@
+import {useEffect, useState} from "react";
+import {WinnerType} from "../utils/types.ts";
+
 function Winners() {
+    const [winners, setWinners] = useState<WinnerType[]>([])
+
+    useEffect(() => {
+        fetch("http://127.0.0.1:3000/winners")
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+            })
+    }, []);
+
     return (
         <></>
     )
